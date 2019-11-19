cb4f2c21af32fadbd47f5555795cc28c8b656af7
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-11-19 15:02:43 +0900

    タイトル改良

---
+++ Street Light/Assets/Script/Suzuki/Dark_ctr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dark_ctr : MonoBehaviour
+{
+    Animator animator;
+
+    int dark_count;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        dark_count++;
+        if (dark_count == 180)
+        {
+            animator.SetTrigger("DarkTrigger");
+            dark_count = 0;
+        }
+    }
+}
---
+++ Street Light/Assets/Script/Suzuki/Title_Light_ctr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_Light_ctr : MonoBehaviour
+{
+    Animator animator;
+
+    int anime_count;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anime_count++;
+        if (anime_count == 180)
+        {
+            animator.SetTrigger("FlashTrigger");
+            anime_count = 0;
+        }
+    }
+}
