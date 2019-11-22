ca17c410d3f921e8911c970874d7ff223642eeef
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-11-22 15:01:16 +0900

    タイトル改良

--- Street Light/Assets/Script/Suzuki/Title_Light_ctr.cs
+++ Street Light/Assets/Script/Suzuki/Title_Light_ctr.cs
@@ -8,20 +8,38 @@ public class Title_Light_ctr : MonoBehaviour
 
     int anime_count;
 
+    float scale_x;
+    float scale_y;
+    float scale_ctr;
+
     // Use this for initialization
     void Start ()
     {
         animator = GetComponent<Animator>();
+        scale_x = transform.localScale.x;
+        scale_y = transform.localScale.y;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         anime_count++;
+        
         if (anime_count == 180)
         {
             animator.SetTrigger("FlashTrigger");
             anime_count = 0;
         }
+
+        if (scale_ctr <= 0)
+        {
+            scale_ctr += 0.002f;
+            if (scale_ctr >= 0.12)
+            {
+                scale_ctr -= 0.002f;
+            }
+        }
+        
+        transform.localScale = new Vector3(scale_x += scale_ctr, scale_y += scale_ctr, 1);
     }
 }
