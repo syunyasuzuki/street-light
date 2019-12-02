c27bff536dbdfa7eb13a9a25fce1f711b86124b1
hasegawaki <hk.lucy.0424@gmail.com>
2019-12-02 09:14:30 +0900

    光の当たり判定を改善、なおフレーム処理しないため見た目と内側がずれてる

--- Street Light/Assets/Script/Hasegawa/Map.cs
+++ Street Light/Assets/Script/Hasegawa/Map.cs
@@ -37,6 +37,7 @@ public class Map : MonoBehaviour {
     //マップの書き変え
     public void Rewrite_map(int px,int py,int num){
         Debug.Log("もらった値　X："+ px + "　Y："+ py + "　NUM："+ num + "　計算した値　mainmap y "+ map_position_y[py] + "　mainmap x "+ (px+map_position_x));
+        if (px + map_position_x < Mapsize_x || map_position_y[py] >= Mapsize_y) { }
         mainmap[map_position_y[py], px + map_position_x] = num;
         
     }
@@ -52,120 +53,120 @@ public class Map : MonoBehaviour {
         float sub_y = y - Py;//座標0に合わせたX
 
         //正規化した座標から光に当たっているかどうかを求める
-        switch (mainmap[map_position_y[Py], Px + map_position_x]){
-            //全部光
+        switch (mainmap[map_position_y[Py], Px + map_position_x]) {
+            //すべて光
             case 1:return 0;
-            //左側から右側へ光が増える
+            //右によっていく
             case 2:
-                if (sub_x <= -0.4) { return 0; }
+                if (sub_x >= -0.4f) { return 0; }
                 break;
             case 3:
-                if (sub_x <= -0.3) { return 0; }
+                if (sub_x >= -0.3f) { return 0; }
                 break;
             case 4:
-                if (sub_x <= -0.2) { return 0; }
+                if (sub_x >= -0.2f) { return 0; }
                 break;
             case 5:
-                if (sub_x <= -0.1) { return 0; }
+                if (sub_x >= -0.1f) { return 0; }
                 break;
             case 6:
-                if (sub_x <= 0) { return 0; }
+                if (sub_x >= 0.0f) { return 0; }
                 break;
             case 7:
-                if (sub_x <= 0.1) { return 0; }
+                if (sub_x >= 0.1f) { return 0; }
                 break;
             case 8:
-                if (sub_x <= 0.2) { return 0; }
+                if (sub_x >= 0.2f) { return 0; }
                 break;
             case 9:
-                if (sub_x <= 0.3) { return 0; }
+                if (sub_x >= 0.3f) { return 0; }
                 break;
             case 10:
-                if (sub_x <= 0.4) { return 0; }
+                if (sub_x >= 0.4f) { return 0; }
                 break;
-            //右側から左側へ光が増える
+            //左によっていく
             case 11:
-                if (sub_x >= 0.4) { return 0; }
+                if (sub_x <= 0.4f) { return 0; }
                 break;
             case 12:
-                if (sub_x >= 0.3) { return 0; }
+                if (sub_x <= 0.3f) { return 0; }
                 break;
             case 13:
-                if (sub_x >= 0.2) { return 0; }
+                if (sub_x <= 0.2f) { return 0; }
                 break;
             case 14:
-                if (sub_x >= 0.1) { return 0; }
+                if (sub_x <= 0.1f) { return 0; }
                 break;
             case 15:
-                if (sub_x >= 0) { return 0; }
+                if (sub_x <= 0.0f) { return 0; }
                 break;
             case 16:
-                if (sub_x >= -0.1) { return 0; }
+                if (sub_x <= -0.1f) { return 0; }
                 break;
             case 17:
-                if (sub_x >= -0.2) { return 0; }
+                if (sub_x <= -0.2f) { return 0; }
                 break;
             case 18:
-                if (sub_x >= -0.3) { return 0; }
+                if (sub_x <= -0.3f) { return 0; }
                 break;
             case 19:
-                if (sub_x >= -0.4) { return 0; }
+                if (sub_x <= -0.4f) { return 0; }
                 break;
-            //下から上へ光が増える
+            //上によっていく
             case 20:
-                if (sub_y <= -0.4) { return 0; }
+                if (sub_y >= -0.4f) { return 0; }
                 break;
             case 21:
-                if (sub_y <= -0.3) { return 0; }
+                if (sub_y >= -0.3f) { return 0; }
                 break;
             case 22:
-                if (sub_y <= -0.2) { return 0; }
+                if (sub_y >= -0.2f) { return 0; }
                 break;
             case 23:
-                if (sub_y <= -0.1) { return 0; }
+                if (sub_y >= -0.1f) { return 0; }
                 break;
             case 24:
-                if (sub_y <= 0) { return 0; }
+                if (sub_y >= 0.0f) { return 0; }
                 break;
             case 25:
-                if (sub_y <= 0.1) { return 0; }
+                if (sub_y >= 0.1f) { return 0; }
                 break;
             case 26:
-                if (sub_y <= 0.2) { return 0; }
+                if (sub_y >= 0.2f) { return 0; }
                 break;
             case 27:
-                if (sub_y <= 0.3) { return 0; }
+                if (sub_y >= 0.3f) { return 0; }
                 break;
             case 28:
-                if (sub_y <= 0.4) { return 0; }
+                if (sub_y >= 0.4f) { return 0; }
                 break;
-            //上から下へ光が増える
+            //下によっていく
             case 29:
-                if (sub_y >= 0.4) { return 0; }
+                if (sub_y <= 0.4f) { return 0; }
                 break;
             case 30:
-                if (sub_y >= 0.3) { return 0; }
+                if (sub_y <= 0.3f) { return 0; }
                 break;
             case 31:
-                if (sub_y >= 0.2) { return 0; }
+                if (sub_y <= 0.2f) { return 0; }
                 break;
             case 32:
-                if (sub_y >= 0.1) { return 0; }
+                if (sub_y <= 0.1f) { return 0; }
                 break;
             case 33:
-                if (sub_y >= 0) { return 0; }
+                if (sub_y <= 0.0f) { return 0; }
                 break;
             case 34:
-                if (sub_y >= -0.1) { return 0; }
+                if (sub_y <= 0.1f) { return 0; }
                 break;
             case 35:
-                if (sub_y >= -0.2) { return 0; }
+                if (sub_y <= 0.2f) { return 0; }
                 break;
             case 36:
-                if (sub_y >= -0.3) { return 0; }
+                if (sub_y <= 0.3f) { return 0; }
                 break;
             case 37:
-                if (sub_y >= -0.4) { return 0; }
+                if (sub_y <= 0.4f) { return 0; }
                 break;
         }
         return 1;
@@ -190,7 +191,7 @@ public class Map : MonoBehaviour {
         }
 
         count = 0;
-        //ライト、障害物、電源を配置する
+        //ライト、障害物を配置する
         foreach(GameObject go in Light_Prfb){
             count++;
             GameObject subgo = Instantiate(go) as GameObject;
