ecf7b9410cf4e6812fc52199b4f607cb0e3d3b9a
hasegawaki <hk.lucy.0424@gmail.com>
2019-12-02 10:44:32 +0900

    スクリプト調整

--- Street Light/Assets/Script/Hasegawa/Map.cs
+++ Street Light/Assets/Script/Hasegawa/Map.cs
@@ -19,8 +19,8 @@ public class Map : MonoBehaviour {
     //マップ（0 = 暗闇）
     int[,] mainmap = new int[Mapsize_y, Mapsize_x];
 
-    int map_position_x = 0; //座標から配列へ直す値X
-    int[] map_position_y = new int [Mapsize_y];//座標から配列へ直す値Y
+    int map_position_x = Mapsize_x / 2; //座標から配列へ直す値X
+    int map_position_y = Mapsize_y - 1;//座標から配列へ直す値Y
 
     //------------------------------------------------------------
 
@@ -36,9 +36,9 @@ public class Map : MonoBehaviour {
 
     //マップの書き変え
     public void Rewrite_map(int px,int py,int num){
-        Debug.Log("もらった値　X："+ px + "　Y："+ py + "　NUM："+ num + "　計算した値　mainmap y "+ map_position_y[py] + "　mainmap x "+ (px+map_position_x));
-        if (px + map_position_x < Mapsize_x || map_position_y[py] >= Mapsize_y){
-            mainmap[map_position_y[py], px + map_position_x] = num;
+        //Debug.Log("もらった値　X："+ px + "　Y："+ py + "　NUM："+ num + "　計算した値　mainmap y "+ map_position_y[py] + "　mainmap x "+ (px+map_position_x));
+        if (px + map_position_x < Mapsize_x || (map_position_y - py < Mapsize_y && map_position_y - py >= 0)){
+            mainmap[map_position_y - py, px + map_position_x] = num;
         }
         
     }
@@ -54,135 +54,135 @@ public class Map : MonoBehaviour {
         float sub_y = y - Py;//座標0に合わせたX
 
         //正規化した座標から光に当たっているかどうかを求める
-        switch (mainmap[map_position_y[Py], Px + map_position_x]) {
-            //すべて光
-            case 1:return 0;
-            //右によっていく
-            case 2:
-                if (sub_x >= -0.4f) { return 0; }
-                break;
-            case 3:
-                if (sub_x >= -0.3f) { return 0; }
-                break;
-            case 4:
-                if (sub_x >= -0.2f) { return 0; }
-                break;
-            case 5:
-                if (sub_x >= -0.1f) { return 0; }
-                break;
-            case 6:
-                if (sub_x >= 0.0f) { return 0; }
-                break;
-            case 7:
-                if (sub_x >= 0.1f) { return 0; }
-                break;
-            case 8:
-                if (sub_x >= 0.2f) { return 0; }
-                break;
-            case 9:
-                if (sub_x >= 0.3f) { return 0; }
-                break;
-            case 10:
-                if (sub_x >= 0.4f) { return 0; }
-                break;
-            //左によっていく
-            case 11:
-                if (sub_x <= 0.4f) { return 0; }
-                break;
-            case 12:
-                if (sub_x <= 0.3f) { return 0; }
-                break;
-            case 13:
-                if (sub_x <= 0.2f) { return 0; }
-                break;
-            case 14:
-                if (sub_x <= 0.1f) { return 0; }
-                break;
-            case 15:
-                if (sub_x <= 0.0f) { return 0; }
-                break;
-            case 16:
-                if (sub_x <= -0.1f) { return 0; }
-                break;
-            case 17:
-                if (sub_x <= -0.2f) { return 0; }
-                break;
-            case 18:
-                if (sub_x <= -0.3f) { return 0; }
-                break;
-            case 19:
-                if (sub_x <= -0.4f) { return 0; }
-                break;
-            //上によっていく
-            case 20:
-                if (sub_y >= -0.4f) { return 0; }
-                break;
-            case 21:
-                if (sub_y >= -0.3f) { return 0; }
-                break;
-            case 22:
-                if (sub_y >= -0.2f) { return 0; }
-                break;
-            case 23:
-                if (sub_y >= -0.1f) { return 0; }
-                break;
-            case 24:
-                if (sub_y >= 0.0f) { return 0; }
-                break;
-            case 25:
-                if (sub_y >= 0.1f) { return 0; }
-                break;
-            case 26:
-                if (sub_y >= 0.2f) { return 0; }
-                break;
-            case 27:
-                if (sub_y >= 0.3f) { return 0; }
-                break;
-            case 28:
-                if (sub_y >= 0.4f) { return 0; }
-                break;
-            //下によっていく
-            case 29:
-                if (sub_y <= 0.4f) { return 0; }
-                break;
-            case 30:
-                if (sub_y <= 0.3f) { return 0; }
-                break;
-            case 31:
-                if (sub_y <= 0.2f) { return 0; }
-                break;
-            case 32:
-                if (sub_y <= 0.1f) { return 0; }
-                break;
-            case 33:
-                if (sub_y <= 0.0f) { return 0; }
-                break;
-            case 34:
-                if (sub_y <= 0.1f) { return 0; }
-                break;
-            case 35:
-                if (sub_y <= 0.2f) { return 0; }
-                break;
-            case 36:
-                if (sub_y <= 0.3f) { return 0; }
-                break;
-            case 37:
-                if (sub_y <= 0.4f) { return 0; }
-                break;
+        if (map_position_y - Py < Mapsize_y && map_position_y - Py >= 0){
+            switch (mainmap[map_position_y - Py, Px + map_position_x]){
+                //すべて光
+                case 1: return 0;
+                //右によっていく
+                case 2:
+                    if (sub_x >= -0.4f) { return 0; }
+                    break;
+                case 3:
+                    if (sub_x >= -0.3f) { return 0; }
+                    break;
+                case 4:
+                    if (sub_x >= -0.2f) { return 0; }
+                    break;
+                case 5:
+                    if (sub_x >= -0.1f) { return 0; }
+                    break;
+                case 6:
+                    if (sub_x >= 0.0f) { return 0; }
+                    break;
+                case 7:
+                    if (sub_x >= 0.1f) { return 0; }
+                    break;
+                case 8:
+                    if (sub_x >= 0.2f) { return 0; }
+                    break;
+                case 9:
+                    if (sub_x >= 0.3f) { return 0; }
+                    break;
+                case 10:
+                    if (sub_x >= 0.4f) { return 0; }
+                    break;
+                //左によっていく
+                case 11:
+                    if (sub_x <= 0.4f) { return 0; }
+                    break;
+                case 12:
+                    if (sub_x <= 0.3f) { return 0; }
+                    break;
+                case 13:
+                    if (sub_x <= 0.2f) { return 0; }
+                    break;
+                case 14:
+                    if (sub_x <= 0.1f) { return 0; }
+                    break;
+                case 15:
+                    if (sub_x <= 0.0f) { return 0; }
+                    break;
+                case 16:
+                    if (sub_x <= -0.1f) { return 0; }
+                    break;
+                case 17:
+                    if (sub_x <= -0.2f) { return 0; }
+                    break;
+                case 18:
+                    if (sub_x <= -0.3f) { return 0; }
+                    break;
+                case 19:
+                    if (sub_x <= -0.4f) { return 0; }
+                    break;
+                //上によっていく
+                case 20:
+                    if (sub_y >= -0.4f) { return 0; }
+                    break;
+                case 21:
+                    if (sub_y >= -0.3f) { return 0; }
+                    break;
+                case 22:
+                    if (sub_y >= -0.2f) { return 0; }
+                    break;
+                case 23:
+                    if (sub_y >= -0.1f) { return 0; }
+                    break;
+                case 24:
+                    if (sub_y >= 0.0f) { return 0; }
+                    break;
+                case 25:
+                    if (sub_y >= 0.1f) { return 0; }
+                    break;
+                case 26:
+                    if (sub_y >= 0.2f) { return 0; }
+                    break;
+                case 27:
+                    if (sub_y >= 0.3f) { return 0; }
+                    break;
+                case 28:
+                    if (sub_y >= 0.4f) { return 0; }
+                    break;
+                //下によっていく
+                case 29:
+                    if (sub_y <= 0.4f) { return 0; }
+                    break;
+                case 30:
+                    if (sub_y <= 0.3f) { return 0; }
+                    break;
+                case 31:
+                    if (sub_y <= 0.2f) { return 0; }
+                    break;
+                case 32:
+                    if (sub_y <= 0.1f) { return 0; }
+                    break;
+                case 33:
+                    if (sub_y <= 0.0f) { return 0; }
+                    break;
+                case 34:
+                    if (sub_y <= 0.1f) { return 0; }
+                    break;
+                case 35:
+                    if (sub_y <= 0.2f) { return 0; }
+                    break;
+                case 36:
+                    if (sub_y <= 0.3f) { return 0; }
+                    break;
+                case 37:
+                    if (sub_y <= 0.4f) { return 0; }
+                    break;
+            }
+            return 1;
         }
-        return 1;
+        else{
+            if (Py == Mapsize_y && Px == 0) { return 0; }
+            else { return 1; }
+        }
     }
 
     //-----------------------------------------------------------
 
 	// Use this for initialization
 	void Start () {
-
-        //マップの大きさをもとに配置位置のプリセットを作る
-        map_position_x = Mapsize_x / 2;
-        for(int y = Mapsize_y -1 ; y >= 0; y--){
-            map_position_y[y] = y;
-        }
 
 		//mainmapを0クリアする（すべて暗闇にする）
         for(int y = 0; y < Mapsize_y; y++){
