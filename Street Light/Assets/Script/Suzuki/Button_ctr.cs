76782d8945fa442ecd157bb28502939a8dff5957
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-12-09 16:00:09 +0900

    スクリプトまとめた

--- Street Light/Assets/Script/Suzuki/Button_ctr.cs
+++ Street Light/Assets/Script/Suzuki/Button_ctr.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SelectManager : MonoBehaviour {
+public class Button_ctr : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
+    //ボタンを押したら呼び出すメソッド
+    public void Go_select()
+    {
+        //セレクトシーンに移動
+        SceneManager.LoadScene("SelectScene");
+    }
+
+    //ボタンを押したら呼び出すメソッド
+    public void Go_Title()
+    {
+        //タイトルシーンに移動
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    //-----------------------GameScene---------------------------//
     //ボタンを押したら呼び出すメソッド
     public void Game1()
     {
@@ -35,4 +53,5 @@ public class SelectManager : MonoBehaviour {
         //ステージ1-3に移動
         SceneManager.LoadScene("Stage3");
     }
+    //--------------------------------------------------------------//
 }
