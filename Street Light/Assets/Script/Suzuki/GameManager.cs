9ff318a758d3ff5a7cea6785dd9713eec7a42573
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-12-10 15:57:02 +0900

    メニュー作成

---
+++ Street Light/Assets/Script/Suzuki/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Button_ctr
+{
+    [SerializeField] GameObject menu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        menu.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            menu.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+	}
+
+    public void Go_Title()
+    {
+        base.Go_Title();
+    }
+
+    public void Go_Select()
+    {
+        base.Go_select();
+    }
+}
