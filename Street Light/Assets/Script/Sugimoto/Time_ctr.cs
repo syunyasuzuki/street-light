c2effb4f675cbe3380695ec0c94e1fb9e23a0f0f
sugimotoshunsuke <nkc.sixyunnsuke.sugimoto@gmail.com>
2019-12-10 14:55:52 +0900

    Timeのスクリプト編集

--- Street Light/Assets/Script/Sugimoto/Time_ctr.cs
+++ Street Light/Assets/Script/Sugimoto/Time_ctr.cs
@@ -2,33 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Time_ctr : MonoBehaviour
 {
-
-    public int minute = 0;         //分
-    public float seconds = 0.0f;   //秒
-
+    public static int minute;         //分
+    public static float seconds;   //秒
+    public float beforeSeconds;
     public Text timetext;
 
     // Use this for initialization
     void Start()
     {
-
+        minute = 0;
+        seconds = 0f;
+        beforeSeconds = 0f;
         //Text取得
         timetext = GetComponent<Text>();
 
-    }
 
+    }
 
     // Update is called once per frame
     void Update()
     {
-
         //時間加算
         seconds += Time.deltaTime;
 
-        //時間表示
-        timetext.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        //分になったら秒を0にする
+        if (seconds >= 60f)
+        {
+            minute++;
+            seconds = seconds - 60;
+        }
+        //値が変わった時テキストを更新
+        if ((int)seconds != (int)beforeSeconds)
+        {
+            //時間表示
+            timetext.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
+        beforeSeconds = seconds;
+
     }
+    ////テスト
+    ////　スタートボタンを押したら実行
+    //public void OnClickGameStart()
+    //{
+    //    SceneManager.LoadScene("Stage2");
+    //}
+    
+    //void Awake()
+    //{
+    //    DontDestroyOnLoad(gameObject);
+    //}
 }
