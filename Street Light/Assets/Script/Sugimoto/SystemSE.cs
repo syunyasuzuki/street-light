20aeef4666b09fe8948da51ca84e815d0d86a4aa
sugimotoshunsuke <nkc.sixyunnsuke.sugimoto@gmail.com>
2019-12-17 14:07:51 +0900

    システム音追加

---
+++ Street Light/Assets/Script/Sugimoto/SystemSE.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemSE : MonoBehaviour {
+
+    AudioSource audioSource;
+    public AudioClip SE;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //再生
+    public void PushButton()
+    {
+        audioSource.PlayOneShot(SE);
+    }
+}
