fa41b9694a5a041f6bcedd7461b053411e1ad5bd
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-12-17 16:08:03 +0900

    クリア実装

--- Street Light/Assets/Script/Suzuki/GameManager.cs
+++ Street Light/Assets/Script/Suzuki/GameManager.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameManager : Button_ctr
 {
     public GameObject menu;
     public GameObject player;
+
+    [SerializeField] Button List1;
+    [SerializeField] Button List2;
+    [SerializeField] Button List3;
 
 	// Use this for initialization
 	void Start ()
@@ -20,6 +25,7 @@ public class GameManager : Button_ctr
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             menu.SetActive(true);
+            List1.Select();
             Time.timeScale = 0.0f;
             player.GetComponent<player_ctr>().enabled = false;
         }
